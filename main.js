1e1eb9de5aae4fe8f384aa43e3a7be6ce0704305
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-03-12 09:00:42 +0500

    todo added flash

--- main.js
+++ main.js
@@ -6,17 +6,24 @@ document.querySelector(`#submitButton`).onclick = (event) => {
   data = document.querySelector(`#todo`).value;
   console.log(data);
   let error = document.querySelector(`#error`);
-  if (data.length < 5) {
-    error.innerText = "sentence is too short";
+
+  if (data.length <= 3) {
+    error.innerText = "please add some more text";
 
     error.style.color = "red";
   } else {
-    error.innerText = "";
+    // error.innerText = "";
+    error.style.color = "green";
+    error.innerText = "todo added";
     todos.push(data);
     console.log(todos);
 
     const li = document.createElement("li");
     li.textContent = data;
     todoItem.appendChild(li);
+    setTimeout(() => {
+      error.innerText = "";
+    }, 300);
+    document.querySelector(`input`).value = "";
   }
 };
